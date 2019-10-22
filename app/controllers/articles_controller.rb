6abe7bdffe9c465158b6674f38c2ba9ecd9292a1
lakshmi535 <lakshmikasireddy535@gmail.com>
2019-10-22 11:43:47 +0530

    mvcproject

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+	  http_basic_authenticate_with name: "ruby", password: "railes", except: [:index, :show]
+	
+	def create
+		#render plain: params[:article].inspect
+		@article=Article.new(params.require(:article).permit(:title,:text))
+		if  @article.save
+			redirect_to @article
+		else
+		 	render 'new'
+		end
+	end	 		
+	def show
+		@article=Article.find(params[:id])
+	end	
+	def index
+		@articles=Article.all
+	end	
+	def new
+		@article=Article.new
+	end	
+	def edit
+		@article=Article.find(params[:id])
+	end
+	def update
+		@article=Article.new(params.require(:article).permit(:title,:text))
+		if  @article.save
+			redirect_to @article
+		else
+		 	render 'edit'
+		end
+	end
+	def destroy
+		@article=Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end		
+end	
